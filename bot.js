25772bebe43942614c064bcdf18f76848e502d58
Hybh <44950520+Bdon8Ism@users.noreply.github.com>
2018-12-13 13:57:19 -0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -60,20 +60,20 @@ message.channel.send('#daily')
 ///////////////////////////////////////////////
 
 bot1.on(`ready`, () => {
- 
-    
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot1.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-  }
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
 
+    }
+  
 });
+
 ///////////////////////////////////////
 
 bot1.on('message', message => {
@@ -135,16 +135,18 @@ message.channel.send('#daily')
 ////////////////////////////////
 
 bot2.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot2.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 
 /////////////////////////////////////
@@ -204,16 +206,18 @@ message.channel.send('#daily')
 ////////////////////////////////////
 
 bot3.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot3.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 //////////////////////////////////
 
@@ -278,16 +282,18 @@ message.channel.send('#daily')
 ////////////////////////////////////
 
 bot4.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot4.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 //////////////////////////////////
 
@@ -352,16 +358,18 @@ message.channel.send('#daily')
 ////////////////////////////////////
 
 bot5.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot5.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 //////////////////////////////////
 
@@ -426,16 +434,18 @@ message.channel.send('#daily')
 ////////////////////////////////////
 
 bot6.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot6.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 //////////////////////////////////
 
@@ -500,16 +510,18 @@ message.channel.send('#daily')
 ////////////////////////////////////
 
 bot7.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot7.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 //////////////////////////////////
 
@@ -574,16 +586,18 @@ message.channel.send('#daily')
 ////////////////////////////////////
 
 bot8.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot8.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 //////////////////////////////////
 
@@ -648,16 +662,18 @@ message.channel.send('#daily')
 ////////////////////////////////////
 
 bot9.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot9.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 //////////////////////////////////
 
@@ -722,16 +738,18 @@ message.channel.send('#daily')
 ////////////////////////////////////
 
 bot10.on(`ready`, () => {
-   
-        for(let x = 0; x < 900; x++) {
-
-  
+  let count = 0;
+  let ecount = 0;
   let channel = bot10.guilds.get("520545293096189952").channels.find(c => c.id == "520545626778501130");
-   setInterval(function() {
-    channel.send(`**Hi I Love Palestaine <3** [ " **__ ${x} __** " ]`)
-  }, 900);
 
-}
+  for(let x = 0; x < 90000; x++) {
+    channel.send(`** I Love Palestaine :heart: **[ "**__ ${x} __** " ]`)
+      .then(m => {
+        count++;
+      })
+
+    }
+  
 });
 //////////////////////////////////
 
